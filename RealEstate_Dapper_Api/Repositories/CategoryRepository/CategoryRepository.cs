f69ee5bf5c3fe90ae582f452052263279371a034
Nazli2 <deniznazlisahin@gmail.com>
2025-10-21 17:37:20 +0300

    Ders 6 - Kategorilerin Eklenmesi ve ExecuteAsync

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using RealEstate_Dapper_Api.Dtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+using Dapper;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly Context _context;
+
+    public CategoryRepository(Context context)
+    {
+        _context = context;
+    }
+
+    public async void CreateCategory(CreateCategoryDto createCategoryDto)
+    {
+        string query = "Insert Into Category (CategoryName,CategoryStatus) Values (@categoryName, @categoryStatus)";
+        var parameters = new DynamicParameters();
+        parameters.Add("@categoryName", createCategoryDto.CategoryName);
+        parameters.Add("@categoryStatus", true);
+        using (var connection = _context.CreateConnection())
+        {
+            await connection.ExecuteAsync(query, parameters);
+        }
+    }
+
+    public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+    {
+        string query = "Select * From Category";
+        using (var connection = _context.CreateConnection())
+        {
+            var values = await connection.QueryAsync<ResultCategoryDto>(query);
+            return values.ToList();
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.Dtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+public interface ICategoryRepository
+{
+    Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+    void CreateCategory(CreateCategoryDto createCategoryDto);
+}
