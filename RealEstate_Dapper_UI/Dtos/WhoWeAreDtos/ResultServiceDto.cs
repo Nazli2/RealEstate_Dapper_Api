875c24976a69acec17171625d6495b2b3b1fd70c
codenestlab-creator <codenestlabb@gmail.com>
2025-10-31 14:31:47 +0300

    #16 -  Api ile Tek Satır Veri Consume Etme

---
+++ RealEstate_Dapper_UI/Dtos/WhoWeAreDtos/ResultServiceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDtos;
+
+public class ResultServiceDto
+{
+    public int ServiceID { get; set; }
+    public int ServiceName { get; set; }
+    public bool ServiceStatus { get; set; }
+}
